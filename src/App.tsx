58c15246504e0ec7a665152b41a7cdc8a95a9faa
RomanB <djimbik93@gmail.com>
2023-04-25 10:14:41 +0300

    todo-list start

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+function App() {
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+    return (
+        <div className="App">
+            <Todolist tasks={tasks1} title={'What to learn'}/>
+            <Todolist tasks={tasks2} title={'Songs'}/>
+        </div>
+    );
+}
+
+export default App;
